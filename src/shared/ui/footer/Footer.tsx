9353a58c319f7447acc8400efd37828fb940c4d7
Marshanishvili Mykhailo <sozure.synergy@gmail.com>
2024-07-23 14:51:04 +0200

    FIX imports

--- src/shared/ui/footer/Footer.tsx
+++ src/shared/ui/footer/Footer.tsx
@@ -1,31 +1,25 @@
 import { footerData } from '@/shared/data'
 import { FooterColumn } from '@/shared/ui'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { useTranslation } from 'react-i18next'
 import styles from './footer.module.scss'
 
 export const Footer = () => {
 	const { i18n } = useTranslation()
-	const [isMounted, setIsMounted] = useState(false)
-
-	useEffect(() => {
-		setIsMounted(true)
-	}, [])
 
 	return (
 		<footer className={styles.footer}>
 			<div className={styles.footer_column_container}>
-				{isMounted &&
-					footerData.map(
-						(data, index) =>
-							data.language === i18n.language && (
-								<React.Fragment key={index}>
-									<FooterColumn footerData={data.footer.socialMedia} />
-									<FooterColumn footerData={data.footer.navigation} />
-									<FooterColumn footerData={data.footer.papers} />
-								</React.Fragment>
-							)
-					)}
+				{footerData.map(
+					(data, index) =>
+						data.language === i18n.language && (
+							<React.Fragment key={index}>
+								<FooterColumn footerData={data.footer.socialMedia} />
+								<FooterColumn footerData={data.footer.navigation} />
+								<FooterColumn footerData={data.footer.papers} />
+							</React.Fragment>
+						)
+				)}
 			</div>
 		</footer>
 	)
