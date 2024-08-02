6c5ffbaac6d8fdcc5fb24e43ff6e01c4ff635cf4
Marshanishvili Mykhailo <sozure.synergy@gmail.com>
2024-08-02 15:01:28 +0200

    FIX conflict

--- src/shared/ui/logo/Logo.tsx
+++ src/shared/ui/logo/Logo.tsx
@@ -6,15 +6,10 @@ import styles from './logo.module.scss'
 export const Logo: FC = () => {
 	return (
 		<>
-<<<<<<< HEAD
-			<Link href={'/'} className={'cursor-pointer'}>
-				<Heading level={4}>MHID</Heading>
-=======
 			<Link href={'/'}>
 				<Heading level={4} className={styles.logo}>
 					MHID
 				</Heading>
->>>>>>> 1b6a12e447b30acd43419c8a8abf96b81c07c574
 			</Link>
 		</>
 	)
