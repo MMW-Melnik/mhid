8086f3f2c9504eb70c0bf0b6cd725c3f716e786e
Yaroslav Muzalevkyi <yarik@Yaroslavs-MacBook-Pro.local>
2024-08-13 14:56:25 +0200

    add smooth scroll to the website

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,9 @@ import { useLoading } from '@/shared/hooks'
 import { Loader } from '@/shared/ui'
 import { NextPage } from 'next'
 import { AppProps } from 'next/app'
-import { ReactElement, ReactNode, useEffect } from 'react'
+import { ReactElement, ReactNode, useEffect, useRef } from 'react'
+import Lenis from 'lenis'
+import 'lenis/dist/lenis.css'
 import '../../i18n.config'
 import { DefaultLayout } from '../app/layouts'
 import '../app/styles/index.scss'
@@ -26,6 +28,17 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
 		}
 	}, [isLoading])
 
+	useEffect(() => {
+		const lenis = new Lenis()
+
+		function raf(time: any) {
+			lenis.raf(time)
+			requestAnimationFrame(raf)
+		}
+
+		requestAnimationFrame(raf)
+	}, [isLoading])
+
 	const getLayout =
 		Component.getLayout ??
 		((page: ReactElement) => <DefaultLayout>{page}</DefaultLayout>)
@@ -39,9 +52,8 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
 					}
 				/>
 			)}
-			<div className="content">
-				{!isLoading && getLayout(<Component {...pageProps} />)}
-			</div>
+
+			{!isLoading && getLayout(<Component {...pageProps} />)}
 		</>
 	)
 }
