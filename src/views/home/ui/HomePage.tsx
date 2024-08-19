8881a8b75af4dae331828181fff0fc91b3726368
Yaroslav Muzalevkyi <yarik@Yaroslavs-MacBook-Pro.local>
2024-08-19 16:14:01 +0200

    heading animation

--- src/views/home/ui/HomePage.tsx
+++ src/views/home/ui/HomePage.tsx
@@ -1,6 +1,44 @@
 'use client'
 import React, { useEffect } from 'react'
+import Paragraph from '@/widgets/paragraph/Paragraph'
+import { ContentBlock } from '@/widgets/content-block/ContentBlock'
 
 export const HomePage = () => {
-	return <></>
+	return (
+		<>
+			<ContentBlock>
+				It is worth looking at the era in which our heroines lived through the
+				eyes of a contemporary. For example, a writer. It is known that people
+				in professions perceive the world in a special way and determine their
+				own place in it. Stefan Zweig described the events of the early
+				twentieth century quite vividly. His "Yesterday's World", which can be
+				safely extrapolated to the modern world, leaves no illusions:
+			</ContentBlock>
+			<ContentBlock>
+				It is worth looking at the era in which our heroines lived through the
+				eyes of a contemporary. For example, a writer. It is known that people
+				in professions perceive the world in a special way and determine their
+				own place in it. Stefan Zweig described the events of the early
+				twentieth century quite vividly. His "Yesterday's World", which can be
+				safely extrapolated to the modern world, leaves no illusions:
+			</ContentBlock>
+			<Paragraph>World War 2</Paragraph>
+			<ContentBlock>
+				It is worth looking at the era in which our heroines lived through the
+				eyes of a contemporary. For example, a writer. It is known that people
+				in professions perceive the world in a special way and determine their
+				own place in it. Stefan Zweig described the events of the early
+				twentieth century quite vividly. His "Yesterday's World", which can be
+				safely extrapolated to the modern world, leaves no illusions:
+			</ContentBlock>
+			<ContentBlock>
+				It is worth looking at the era in which our heroines lived through the
+				eyes of a contemporary. For example, a writer. It is known that people
+				in professions perceive the world in a special way and determine their
+				own place in it. Stefan Zweig described the events of the early
+				twentieth century quite vividly. His "Yesterday's World", which can be
+				safely extrapolated to the modern world, leaves no illusions:
+			</ContentBlock>
+		</>
+	)
 }
--- src/widgets/paragraph/Paragraph.tsx
+++ src/widgets/paragraph/Paragraph.tsx
@@ -1,9 +1,49 @@
-import React, { FC } from 'react'
-import { IParagraph } from './paragraph.interface'
+import React, { FC, useEffect, useRef } from 'react'
+import gsap from 'gsap'
+import ScrollTrigger from 'gsap/dist/ScrollTrigger'
 import styles from './paragraph.module.scss'
 
-const Paragraph: FC<IParagraph> = ({ children }) => {
-	return <div className={styles.content}>{children}</div>
+gsap.registerPlugin(ScrollTrigger)
+
+const Paragraph: FC<{ children: React.ReactNode }> = ({ children }) => {
+	const headingRef = useRef<HTMLHeadingElement>(null)
+
+	useEffect(() => {
+		if (!headingRef.current) return
+
+		const tl = gsap.timeline({
+			scrollTrigger: {
+				trigger: headingRef.current,
+				start: 'top 10%',
+				end: '+=700px',
+				pin: headingRef.current,
+				toggleActions: 'play none reverse none',
+				scrub: 2
+			}
+		})
+
+		tl.fromTo(
+			headingRef.current,
+			{ opacity: 0, yPercent: -10 },
+			{ opacity: 1, yPercent: 0, duration: 1 }
+		).to(headingRef.current, { opacity: 0, yPercent: -10, duration: 1 })
+
+		return () => {
+			if (ScrollTrigger.getAll().length > 0) {
+				ScrollTrigger.getAll().forEach(trigger => trigger.kill())
+			}
+		}
+	}, [])
+
+	return (
+		<section className={styles.section}>
+			<div className={styles.content}>
+				<h1 ref={headingRef} className={styles.heading}>
+					{children}
+				</h1>
+			</div>
+		</section>
+	)
 }
 
 export default Paragraph
