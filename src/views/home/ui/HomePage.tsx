dd955c3958833843b09a43959fd64b5b80ec36a8
Yaroslav <166743450+yaroslavmuzalevskyi@users.noreply.github.com>
2024-12-23 12:41:48 +0100

    added images

--- src/views/home/ui/HomePage.tsx
+++ src/views/home/ui/HomePage.tsx
@@ -2,50 +2,13 @@
 import bgImage from '@/app/assets/images/home/bg.jpg'
 import { Chapter, ContentBlock } from '@/widgets/index'
 import { useTranslation } from 'react-i18next'
+import ImageWithSource from '@/shared/ui/image-with-source/ImageWithSource'
+import { Intro } from '@/shared/ui'
 
 import { FormattedText } from '@/shared/ui'
 
 export const HomePage = () => {
 	const { t } = useTranslation('home')
 
-	return (
-		<>
-			<section>
-				<ContentBlock>
-					<FormattedText text={t('chapter_1.text')} />
-				</ContentBlock>
-				<ContentBlock>
-					It&nbsp;was at&nbsp;this time that the protagonists of&nbsp;this
-					project were born in&nbsp;the centre of&nbsp;Europe, albeit
-					in&nbsp;different countries.
-				</ContentBlock>
-			</section>
-
-			<Chapter
-				quote="chapter_2.quote"
-				author="Stefan Zweig. Yesterday's World"
-				img1={bgImage.src}
-				img2={bgImage.src}
-				img3={bgImage.src}
-			>
-				{t('chapter_2.title')}
-			</Chapter>
-			<ContentBlock>
-				It is worth looking at the era in which our heroines lived through the
-				eyes of a contemporary. For example, a writer. It is known that people
-				in professions perceive the world in a special way and determine their
-				own place in it. Stefan Zweig described the events of the early
-				twentieth century quite vividly. His "Yesterday's World", which can be
-				safely extrapolated to the modern world, leaves no illusions:
-			</ContentBlock>
-			<ContentBlock>
-				It is worth looking at the era in which our heroines lived through the
-				eyes of a contemporary. For example, a writer. It is known that people
-				in professions perceive the world in a special way and determine their
-				own place in it. Stefan Zweig described the events of the early
-				twentieth century quite vividly. His "Yesterday's World", which can be
-				safely extrapolated to the modern world, leaves no illusions:
-			</ContentBlock>
-		</>
-	)
+	return <></>
 }
---
+++ src/widgets/chapters/InterwarPeriod/InterwarPeriod.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Chapter } from '@/widgets/paragraph'
+
+const InterwarPeriod = () => {
+	return (
+		<>
+			<Chapter quote="" author="" img1={} img2={} img3={} />
+		</>
+	)
+}
+
+export default InterwarPeriod
