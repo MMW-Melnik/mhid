d937c052211deb28367d554c39012c4199c422d9
Yaroslav <166743450+yaroslavmuzalevskyi@users.noreply.github.com>
2024-12-23 16:14:09 +0100

    updated imageWithSource component and InterwarPeriod chapter is done

--- src/shared/ui/image-with-source/ImageWithSource.tsx
+++ src/shared/ui/image-with-source/ImageWithSource.tsx
@@ -10,29 +10,27 @@ export const ImageWithSource: FC<IImage> = ({
 	alt
 }) => {
 	return (
-		<div className={styles.overlay}>
-			<Tilt
-				className={styles.image_with_source}
-				tiltReverse={false}
-				tiltMaxAngleX={15}
-				tiltMaxAngleY={15}
-				perspective={3000}
-				scale={1.1}
-				transitionSpeed={1000}
-				gyroscope={true}
-				glareEnable={false}
-			>
-				<Image
-					src={sourceImage}
-					width={1024}
-					height={1024}
-					draggable={false}
-					alt={alt}
-					className={styles.image}
-				/>
-				<span className={styles.text}>{sourceText}</span>
-			</Tilt>
-		</div>
+		<Tilt
+			className={styles.image_with_source}
+			tiltReverse={false}
+			tiltMaxAngleX={15}
+			tiltMaxAngleY={15}
+			perspective={3000}
+			scale={1.1}
+			transitionSpeed={1000}
+			gyroscope={true}
+			glareEnable={false}
+		>
+			<Image
+				src={sourceImage}
+				width={1024}
+				height={1024}
+				draggable={false}
+				alt={alt}
+				className={styles.image}
+			/>
+			<span className={styles.text}>{sourceText}</span>
+		</Tilt>
 	)
 }
 
--- src/widgets/chapters/InterwarPeriod/InterwarPeriod.tsx
+++ src/widgets/chapters/InterwarPeriod/InterwarPeriod.tsx
@@ -4,8 +4,10 @@ import img1 from '@/app/assets/images/home/INTERWAR PERIOD/building.jpg'
 import img2 from '@/app/assets/images/home/INTERWAR PERIOD/street1.jpg'
 import img3 from '@/app/assets/images/home/INTERWAR PERIOD/village.jpg'
 import { ContentBlock } from '@/widgets/content-block'
+import { Heading } from '@/shared/ui'
 import { useTranslation } from 'react-i18next'
 import { FormattedText } from '@/shared/ui'
+import ImageWithSource from '@/shared/ui/image-with-source/ImageWithSource'
 
 const InterwarPeriod = () => {
 	const { t } = useTranslation('home')
@@ -22,8 +24,21 @@ const InterwarPeriod = () => {
 				{t('chapter_3.title')}
 			</Chapter>
 			<ContentBlock>
-				<FormattedText text={t('chapter_3.text')} />
+				<FormattedText text={t('chapter_3.text_main')} />
 			</ContentBlock>
+			<Heading level={3}>{t('chapter_3.subtitles.charlotte')}</Heading>
+			<ContentBlock>
+				<FormattedText text={t('chapter_3.text_charlotte')} />
+			</ContentBlock>
+			<Heading level={3}>{t('chapter_3.subtitles.sofia')}</Heading>
+			<ContentBlock>
+				<FormattedText text={t('chapter_3.text_sofia')} />
+			</ContentBlock>
+			<ImageWithSource
+				sourceImage={img1.src}
+				sourceText="helloekfs fdwef wefwefwe"
+				alt="hi"
+			/>
 		</>
 	)
 }
