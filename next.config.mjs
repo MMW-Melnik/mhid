5d4c1803b0bb15a1edb5191e7a5600c2a3d8a481
Yaroslav <166743450+yaroslavmuzalevskyi@users.noreply.github.com>
2025-09-17 13:45:13 +0200

    deploy2

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,4 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
 	poweredByHeader: false,
 	optimizeFonts: true,
@@ -7,7 +6,8 @@ const nextConfig = {
 		APP_URL: process.env.REACT_APP_URL,
 		APP_ENV: process.env.REACT_APP_ENV,
 		FORM_ID: process.env.FORM_ID
-	}
+	},
+	output: 'export' // ✅ tells Next.js to generate static files in /out
 }
 
 export default nextConfig
